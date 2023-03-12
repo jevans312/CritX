3dbcb546da6287017915ff0749d2ad6c8d6ecdea
jevans312 <jevans312@gmail.com>
2023-03-12 15:21:25 -0400

    Clean up header file

--- src/scenes/entities/worldb.h
+++ src/scenes/entities/worldb.h
@@ -11,12 +11,14 @@
 
 // #include "LinearMath/btIDebugDraw.h"
 // #include "../../gl/gldebugdrawer.h"
+
 // #include <sstream>
-#include <omp.h>
+#include "omp.h"
 #include <iomanip>
 // #include <vector>
 // #include <string>
 // #include <cstring> 
+
 #include "../../utils/timer.h"
 #include "../../utils/dir.h"
 #include "../../utils/file.h"
@@ -50,37 +52,38 @@ class WorldB
 		virtual void		init();
 		virtual void		process();
 		virtual btVector3	findPosition();
-		virtual		void makeFloor();
-		float			insertHight;
-		void			getGeneralStats();
-		void			killHalf();
-		void			expireFood();
-		void			expireCritters();
-		void			autoinsertFood();
-		void			autosaveCritters();
-		void			autoexchangeCritters();
-		void			autoinsertCritters();
-		void			eat( CritterB* c );
-		void			procreate( CritterB* c );
-		void			makeDefaultFloor();
+		virtual	void 		makeFloor();
+		float				insertHight;
+		void 				resetCamera();
+		void				getGeneralStats();
+		void				killHalf();
+		void				expireFood();
+		void				expireCritters();
+		void				autoinsertFood();
+		void				autosaveCritters();
+		void				autoexchangeCritters();
+		void				autoinsertCritters();
+		void				eat( CritterB* c );
+		void				procreate( CritterB* c );
+		void				makeDefaultFloor();
 		
-		Settings*		settings;
+		Settings*			settings;
 		Critterselection	*critterselection;
 		Statsbuffer*		statsBuffer;
-		RandGen*		randgen;
+		RandGen*			randgen;
 		Mousepicker*		mousepicker;
-		Dirlayout*		dirlayout;
-		Camera camera;
-		void resetCamera();
+		Dirlayout*			dirlayout;
+		Camera				camera;
 
-		//SpuGatheringCollisionDispatcher*	m_dispatcher;
-		btCollisionDispatcher*	m_dispatcher;
-		btBroadphaseInterface*	m_broadphase;
-		btDefaultCollisionConfiguration* m_collisionConfiguration;
-		btDynamicsWorld*	m_dynamicsWorld;
-		btConstraintSolver*	m_solver;
-// 		SpuMinkowskiPenetrationDepthSolver*	m_solver;
-		btAlignedObjectArray<btCollisionShape*>	m_collisionShapes;
+
+		//SpuGatheringCollisionDispatcher*			m_dispatcher;
+		btCollisionDispatcher*						m_dispatcher;
+		btBroadphaseInterface*						m_broadphase;
+		btDefaultCollisionConfiguration* 			m_collisionConfiguration;
+		btDynamicsWorld*							m_dynamicsWorld;
+		btConstraintSolver*							m_solver;
+// 		SpuMinkowskiPenetrationDepthSolver*			m_solver;
+		btAlignedObjectArray<btCollisionShape*>		m_collisionShapes;
 
 		vector<CritterB*>	critters;
 		vector<Food*>		food;
@@ -88,56 +91,52 @@ class WorldB
 
 		unsigned long		currentCritterID;
 
-		float			freeEnergy;
-		//float			freeEnergyInfo;
+		float				freeEnergy;
 
+				void		drawWithinCritterSight(CritterB *c);
+				void		drawWithinCritterSight(unsigned int cid);
 		virtual void		drawWithGrid();
 		virtual void		drawWithoutFaces();
-		
-		void			drawWithinCritterSight(CritterB *c);
-		void			drawWithinCritterSight(unsigned int cid);
-
 		virtual void		drawfloor();
 		virtual void		childPositionOffset(btVector3* v);
-
-		void			startfoodamount(unsigned int amount);
-
-		void			insertRandomFood(int amount, float energy);
-
 		virtual void		insertCritter();
-// 		void			positionCritterB(unsigned int cid);
-		void			saveAllCritters();
 		virtual void		loadAllCritters();
 		virtual void		loadAllLastSavedCritters();
 
-		void			removeSelectedCritter();
-		void			removeAllSelectedCritters();
-		void			duplicateCritter(unsigned int cid, bool brainmutant, bool bodymutant);
 
-		void			duplicateSelectedCritter();
-		void			spawnBrainMutantSelectedCritter();
-		void			spawnBodyMutantSelectedCritter();
-		void			spawnBrainBodyMutantSelectedCritter();
-		void			feedSelectedCritter();
-		void			resetageSelectedCritter();
+		void				startfoodamount(unsigned int amount);
+		void				insertRandomFood(int amount, float energy);
+		void				saveAllCritters();
 
-		void			duplicateAllSelectedCritters();
-		void			spawnBrainMutantAllSelectedCritters();
-		void			spawnBodyMutantAllSelectedCritters();
-		void			spawnBrainBodyMutantAllSelectedCritters();
 
-		void			killHalfOfCritters();
+		void				removeSelectedCritter();
+		void				removeAllSelectedCritters();
+		void				duplicateCritter(unsigned int cid, bool brainmutant, bool bodymutant);
 
-		void			pickBody(const int& x, const int& y);
-		void			selectBody(const int& x, const int& y);
-		void			castMouseRay();
-		void			movePickedBodyTo();
-		void			movePickedBodyFrom();
-		float			autosaveCounter;
-		float			autoexchangeCounter;
+		void				duplicateSelectedCritter();
+		void				spawnBrainMutantSelectedCritter();
+		void				spawnBodyMutantSelectedCritter();
+		void				spawnBrainBodyMutantSelectedCritter();
+		void				feedSelectedCritter();
+		void				resetageSelectedCritter();
+
+		void				duplicateAllSelectedCritters();
+		void				spawnBrainMutantAllSelectedCritters();
+		void				spawnBodyMutantAllSelectedCritters();
+		void				spawnBrainBodyMutantAllSelectedCritters();
+
+		void				killHalfOfCritters();
+
+		void				pickBody(const int& x, const int& y);
+		void				selectBody(const int& x, const int& y);
+		void				castMouseRay();
+		void				movePickedBodyTo();
+		void				movePickedBodyFrom();
+		float				autosaveCounter;
+		float				autoexchangeCounter;
 
 		// vision
-		unsigned char		*retina;
+		unsigned char*		retina;
 		unsigned int		items;
 
 		const unsigned int*	retinasperrow;
@@ -150,15 +149,12 @@ class WorldB
 		const unsigned int*	threads;
 
 		void			checkCollisions( CritterB* c );
-
-		void grabVision();
-
-		void renderVision();
-
+		void			grabVision();
+		void			renderVision();
 		void			calcMouseDirection();
 		void			moveInMouseDirection(bool towards);
 
-		Dir			dirH;
+		Dir				dirH;
 		Parser*			parseH;
 		File			fileH;
 
@@ -180,23 +176,23 @@ class WorldB
 		void 			camera_rollright();
 
 		// pointers to parents (evolution) mousepos
-		int* mousex;
-		int* mousey;
+		int*			 mousex;
+		int*			 mousey;
 
 		// threading
-		omp_lock_t my_lock1;
-// 		omp_lock_t my_lock2;
+		omp_lock_t		my_lock1;
+// 		omp_lock_t		my_lock2;
+
+
 	private:
 		Raycast*		raycast;
-
 		castResult		mouseRay;
 		btVector3		mouseRayTo;
 		
  		void drawShadow(btScalar* m,const btVector3& extrusion,const btCollisionShape* shape,const btVector3& worldBoundsMin,const btVector3& worldBoundsMax);
 		
-		unsigned int		insertCritterCounter;
+		unsigned int	insertCritterCounter;
 // 		GLDebugDrawer debugDrawer;
-
 
 		// methods
 		inline void		removeCritter(unsigned int cid);
@@ -228,14 +224,13 @@ class WorldB
 
 
 		// vision opts
-		unsigned int picwidth;
-		btScalar drawposition[16];
+		unsigned int 		picwidth;
+		btScalar 			drawposition[16];
 		
-		btManifoldArray   manifoldArray;
+		btManifoldArray   	manifoldArray;
 
-		int			findSelectedCritterID();
-		int			findCritterID(unsigned int cid);
-
+		int					findSelectedCritterID();
+		int					findCritterID(unsigned int cid);
 };
 
 #endif
